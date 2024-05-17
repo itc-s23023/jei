594a342e301a3ac44d51d8bb5905b89d88b466bc
itc-s23023 <s23023@std.it-college.ac.jp>
2024-05-17 11:47:21 +0900

    add 0517

---
+++ d48Q2kadai.java
@@ -0,0 +1,31 @@
+class Animal {
+    public void makeSound() {
+        System.out.println("動物の音");
+    }
+    public void test() {
+        System.out.println("テスト出力");
+    }
+}
+
+class Dog extends Animal {
+    public void makeSound() {
+        System.out.println("わんわん");
+    }
+}
+
+class Cat extends Animal {
+    public void makeSound() {
+        System.out.println("ニャー");
+    }
+}
+
+public class d48Q2kadai {
+    public static void main(String[] args) {
+        Animal a = new Dog();
+         a.makeSound();
+        a.test();
+        Animal b = new Cat();
+        b.makeSound();
+        b.test();
+    }
+}
