ae77c34f4ae4bc77560df1bf1cf2794e86bad649
itc-s23023 <s23023@std.it-college.ac.jp>
2024-07-19 03:24:57 +0000

    add d51Q26kadai

---
+++ d51Q26kadai.java
@@ -0,0 +1,22 @@
+// s23023
+// break追加
+
+public class d51Q26kadai {
+    public static void main(String[] args) {
+        int n = 1;
+        String s = "";
+        switch (n) {
+            case 1:
+                s += "one";
+                break;
+            case 2:
+                s += "two";
+                break;
+            default:
+                s += "?";
+        }
+        System.out.println(s);
+    }
+}
+
+
